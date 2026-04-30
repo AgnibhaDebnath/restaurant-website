e7ec1538558c9fe3c12dc210578c76f98d0ded35
Agnibha Debnath <agnibhadebnath@gmail.com>
2026-04-30 15:36:55 +0530

    feat: add menu items data in menuItems.js

---
+++ src/data/menuItems.js
@@ -0,0 +1,58 @@
+import Rosogolla from "../assets/menu/Rosogolla.jpg"
+import Masala_chai from "../assets/menu/Masala_chai.jpg"
+import Shorshe_Aloo from "../assets/menu/Shorshe_Aloo.jpg"
+import Chicken_Kosha from "../assets/menu/Chicken_Kosha.jpg"
+import Ilish_Bhapa from "../assets/menu/Ilish_Bhapa.png"
+import Mutton_currry from "../assets/menu/Mutton_curry.jpg"
+export const menuItems = [
+    {
+        id: 1,
+        name: "Ilish Bhapa",
+        category:"Fish",
+        price: "₹320",
+        imageURL:Ilish_Bhapa
+    },
+    {
+        id: 2,
+        name: "Chicken Kosha",
+        category:"Chicken",
+        price: "₹280",
+        imageURL:Chicken_Kosha
+    },
+    {
+        id: 3,
+        name: "Shorshe Aloo",
+        category:"Veg",
+        price: "₹140",
+        imageURL:Shorshe_Aloo
+    },
+    {
+        id: 4,
+        name: "Mutton Curry",
+        category:"Mutton",
+        price: "₹350",
+        imageURL:Mutton_currry
+    },  
+    {
+        id: 5,
+        name: "Rosogolla",
+        category:"Desserts",
+        price: "₹30 (per peice)",
+        imageURL:Rosogolla 
+    },
+    {
+        id: 6,
+        name: "Masala Chai",
+        category:"Beverages",
+        price: "₹30 (per cup)",
+        imageURL:Masala_chai
+    },
+    {
+        id: 7,
+        name: "Mutton Kosha",
+        category:"Mutton",
+        price: "₹380",
+        imageURL:""
+    },
+    
+]
