172873c6d96df576af62a2fcb1b67b2860d9f296
Agnibha Debnath <agnibhadebnath@gmail.com>
2026-04-30 17:51:01 +0530

    feat: add category badge to menu cards and update styles

--- src/Components/Menu.jsx
+++ src/Components/Menu.jsx
@@ -27,7 +27,7 @@ const Menu = () => {
         var filteredMenu = menuItems.filter(item => item.category == category);
     }
     return (
-        <section id='menu' className='my-3 bg-[#f9fafb] scroll-mt-20'>
+        <section id='menu' className='bg-[#f9fafb] scroll-mt-20 pt-5 pb-8'>
             <div className='flex justify-center py-3'>
                 <h1 className=' text-4xl min-[450px]:text-5xl text-center font-bold font-[inter]'>Explore Our Menu</h1>
             </div>
@@ -42,11 +42,14 @@ const Menu = () => {
             <div className='w-full grid grid-cols-1 min-[988px]:grid-cols-2 min-[1450px]:grid-cols-3 place-items-center justify-items-center gap-10 px-10'>
                 {filteredMenu.map((item) => (
                     <>
-                        {< div key={item.id} className='w-80 min-[385px]:w-90 min-[432px]:w-100 min-[500px]:w-115 rounded-3xl px-7 min-[500px]:px-10 pt-8 pb-3 bg-white shadow-xl overflow-hidden hover:shadow-2xl' >
+                        {< div key={item.id} className='relative w-80 min-[385px]:w-90 min-[432px]:w-100 min-[500px]:w-115 rounded-4xl px-7 min-[500px]:px-10 pt-8 pb-3 bg-white shadow-xl overflow-hidden hover:shadow-2xl' >
                             <img src={item.imageURL} alt="Menu img" className='w-full h-45 min-[385px]:h-50 min-[432px]:h-60 object-cover rounded-3xl hover:scale-105 transaction duration-200' />
-                            <div className='py-1 min-[432px]:py-3'>
+                            <span className="absolute left-10 top-11 min-[500px]:left-13 bg-[#f59e0b] text-white text-xs px-2 py-1 rounded-xl font-[inter]">
+                                {item.category}
+                            </span>
+                            <div className='py-1 min-[432px]:py-3 flex justify-between pr-4'>
                                 <h1 className='font-extrabold text-2xl font-[inter]'>{item.name}</h1>
-                                <h1 className='font-medium text-[#f59e0b] font-[inter] text-lg'>{item.price}</h1>
+                                <h1 className='font-bold text-[#f59e0b] font-[inter] text-xl'>{item.price}</h1>
                             </div>
                         </div>}
                     </>
