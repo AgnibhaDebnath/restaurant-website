ce09dc109bb842008abd418024221924e1666957
Agnibha Debnath <agnibhadebnath@gmail.com>
2026-05-01 12:36:31 +0530

    style: update styles

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -5,17 +5,17 @@ const Hero = () => {
         <section className='w-full h-screen relative'>
 
 
-            <img src={Hero_image} className='h-full top-0 w-full object-cover ' />
-            <div className="absolute top-0 left-0 w-full h-full bg-black/50"></div>
+            <img src={Hero_image} className='h-full top-0 w-full object-cover object-center' />
+            <div className="absolute top-0 left-0 w-full h-full bg-black/60"></div>
             <div className='absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 w-full '>
                 <div className='flex justify-center '>
                     <h1 className='text-[1.2rem] min-[375px]:text-xl min-[440px]:text-2xl min-[550px]:text-3xl min-[675px]:text-4xl min-[880px]:text-5xl min-[1150px]:text-6xl text-white font-[inter] font-bold text-center'>Authentic Bengali Flavors, Served Fresh</h1>
                 </div>
                 <div className='flex justify-center mt-5 px-1'>
-                    <h2 className='text-center font-medium text-white text-xs min-[575px]:text-sm min-[700px]:text-base min-[810px]:text-lg font-[inter]'>Experience traditional recipes made with love. Book your table and enjoy a true taste of Bengal.</h2>
+                    <h2 className='text-center font-semibold text-white text-xs min-[575px]:text-sm min-[700px]:text-base min-[810px]:text-lg font-[inter]'>Experience traditional recipes made with love. Book your table and enjoy a true taste of Bengal.</h2>
                 </div>
                 <div className='flex justify-center mt-5'>
-                    <button className='px-10 py-2 cursor-pointer hover:scale-105 transition duration-75 rounded-lg text-white font-medium bg-[#f59e0b]'>Book Table</button>
+                    <button className='px-10 py-1.5 cursor-pointer hover:scale-105  rounded-lg text-white font-semibold bg-[#f59e0b] font-[inter] text-lg active:scale-95 transition duration-300'>Book Table</button>
                 </div>
             </div>
         </section>
