43a26e3118e3b1e4d018e9272910e05da8ad91cb
Agnibha Debnath <agnibhadebnath@gmail.com>
2026-05-28 21:02:57 +0530

    style: update about section image and sizing

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -15,14 +15,14 @@ const About = () => {
                         <p className='pl-2 text-base min-[487px]:text-lg min-[1100px]:text-xl max-w-2xl text-gray-600 leading-relaxed'>We bring the rich taste of Bengal to your plate with recipes passed down through generations.</p>
 
                         <p className='pl-2 text-base min-[487px]:text-lg min-[1000px]:text-xl max-w-2xl text-gray-600 leading-relaxed'>Every dish is prepared using fresh ingredients, traditional spices, and a passion for authentic cooking.</p>
-                        <ul className="mt-4 space-y-2 font-medium text-base min-[487px]:text-lg flex  gap-3 flex-col  text-gray-600">
+                        <ul className="mt-4 space-y-2 font-medium text-base min-[487px]:text-lg flex  gap-3 flex-col  text-gray-600 ml-1.5">
                             <li>👨‍🍳 Authentic Recipes</li>
                             <li>🌿 Fresh Ingredients</li>
                             <li>🍛 Traditional Taste</li>
                         </ul>
                     </div>
 
-                    <img src={About_section_image} className='w-90 min-[1100px]:w-100 h-60 min-[1100px]:h-70 object-cover rounded-4xl shadow-2xl  hover:scale-105 transition duration-200' alt="About section image" />
+                    <img src={About_section_image} className='w-76 min-[1100px]:w-100 h-55 min-[1100px]:h-70 object-cover rounded-4xl shadow-2xl  hover:scale-105 transition duration-200' alt="About section image" />
                 </div>
             </div>
         </section>
