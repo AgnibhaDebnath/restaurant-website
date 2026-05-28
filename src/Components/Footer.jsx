54622dccc6d48c62c851cbfdbacba4be56c954ae
Agnibha Debnath <agnibhadebnath@gmail.com>
2026-05-28 23:45:10 +0530

    style: add logo and social media icons to footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,3 +1,7 @@
+import { LuChefHat } from "react-icons/lu";
+import { RiInstagramLine } from "react-icons/ri";
+import { FaXTwitter } from "react-icons/fa6";
+import { RiYoutubeLine } from "react-icons/ri";
 const Footer = () => {
     const handleScroll = (id) => {
         document.getElementById(id).scrollIntoView({
@@ -5,21 +9,32 @@ const Footer = () => {
         });
     };
     return (
-        <footer className="bg-gray-900 text-gray-300 py-10 px-5 font-[inter]">
-            <div className="max-w-6xl mx-auto grid md:grid-cols-3 gap-8">
-                <div className="">
-                    <h2 className="text-2xl font-semibold text-white">
-                        Royal Bengali Kitchen
-                    </h2>
-                    <p className="mt-2 text-base tracking-wide">
+        <footer className="bg-gray-900 text-gray-300 py-14 px-3 min-[400px]:px-5 font-[inter]">
+            <div className="max-w-6xl mx-auto grid md:grid-cols-2 gap-20">
+                <div className="flex flex-col gap-2">
+                    <div className="flex items-center gap-3">
+                        < LuChefHat size={40} className="text-[#f59e0b]" />
+                        <h2 className="text-2xl font-semibold text-white">
+                            Royal Bengali Kitchen
+                        </h2>
+                    </div>
+
+                    <p className="mt-2 text-base tracking-wide pl-13">
                         Authentic flavors, crafted with tradition
                     </p>
+                    <div className="flex gap-5 pl-13 items-center text-white/80">
+                        <RiInstagramLine size={22} className="cursor-pointer hover:text-orange-400 transition-all duration-300" />
+                        <FaXTwitter size={22} className="cursor-pointer hover:text-orange-400" />
+                        <RiYoutubeLine size={26} className="cursor-pointer hover:text-orange-400" />
+                    </div>
                 </div>
 
 
-                <div>
-                    <h2 className="text-white font-semibold mb-2 text-xl">Quick Links</h2>
-                    <ul className="space-y-2 text-lg ml-1.5">
+                <div className="pl-12">
+
+                    <h2 className="text-white font-semibold text-xl mt-2">Quick Links</h2>
+
+                    <ul className="space-y-2 text-lg ml-1.5 mt-1">
                         <li><button aria-label="Go to About section" onClick={() => handleScroll("about")} className="cursor-pointer hover:text-[#f59e0b] font-medium active:scale-95 active:text-[#f59e0b] transition duration-300">About</button></li>
                         <li><button aria-label="Go to Menu section" onClick={() => handleScroll("menu")} className="cursor-pointer hover:text-[#f59e0b] font-medium active:scale-95 active:text-[#f59e0b] transition duration-300">Menu</button></li>
                         <li><button aria-label="Go to Contact section" onClick={() => handleScroll("contact")} className="cursor-pointer hover:text-[#f59e0b] font-medium active:scale-95 active:text-[#f59e0b] transition duration-300">Contact</button></li>
@@ -28,8 +43,8 @@ const Footer = () => {
 
             </div>
 
-            <div className="text-center text-[.9rem] min-[385px]:text-base mt-8 border-t border-gray-700 pt-4">
-                © 2026 Royal Bengali Kitchen. All rights reserved.
+            <div className="flex items-center justify-center text-[.9rem] min-[385px]:text-base mt-8 border-t border-gray-700 pt-4">
+                <h3>© {new Date().getFullYear()} Royal Bengali Kitchen. All rights reserved.</h3>
             </div>
         </footer>
     )
