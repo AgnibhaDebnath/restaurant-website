0c3385d390e5c483f1676e2ba737e9f8fe16af2d
Agnibha Debnath <agnibhadebnath@gmail.com>
2026-05-28 21:11:09 +0530

    style: improve hero section UI

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Hero_image_1 from "../assets/images/Hero_image_1.webp"
 import Hero_image_2 from "../assets/images/Hero_image_2.webp"
+
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import { Autoplay } from "swiper/modules";
@@ -26,8 +27,9 @@ const Hero = () => {
         subHeading: "Enjoy warm ambience, elegant interiors, and memorable dining moments.",
     },
     ]
+    const colorWords = ["Bengali", "Flavors,", "Dining", "Experience"]
     return (
-        <section className='w-full h-[70vh] sm:h-screen relative'>
+        <section className='w-full h-[70vh] sm:h-screen relative overflow-hidden'>
             <Swiper
                 modules={[Autoplay]}
                 autoplay={{ delay: 4000 }}
@@ -37,14 +39,19 @@ const Hero = () => {
                 {heroSlides.map((slide) => (
 
                     <SwiperSlide key={slide.id}>
-                        <img src={slide.image} className={`h-full top-0 w-full object-cover ${slide.id == 1 ? "object-[70%_25%] sm:object-[center_70%]" : "object-[40%_center]"}`} />
-                        <div className="absolute top-0 left-0 w-full h-full bg-black/60"></div>
-                        <div className='absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 w-full '>
+                        <img src={slide.image} className={`h-full top-0 w-full object-cover absolute inset-0 ${slide.id == 1 ? "object-[70%_25%] sm:object-[center_70%]" : "object-[40%_center]"}`} />
+                        <div className="absolute inset-0 bg-black/60 z-10"></div>
+                        <div className='absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 w-full z-20'>
                             <div className='flex justify-center '>
-                                <h1 className='text-[1.2rem] min-[375px]:text-xl min-[440px]:text-2xl min-[550px]:text-3xl min-[675px]:text-4xl min-[880px]:text-5xl min-[1150px]:text-6xl text-white font-[inter] font-bold text-center'>{slide.heading}</h1>
+                                <h1 className='text-[1.2rem] min-[375px]:text-xl min-[440px]:text-2xl min-[550px]:text-3xl min-[675px]:text-4xl min-[880px]:text-5xl min-[1150px]:text-6xl text-white font-[inter] font-bold text-center'>{slide.heading.split(" ").map((word, index) => (
+                                    <span key={index} className={`${colorWords.includes(word) ? "text-[#f59e0b]" : ""}`}>
+                                        {console.log(word)}
+                                        {word}{" "}
+                                    </span>
+                                ))}</h1>
                             </div>
                             <div className='flex justify-center mt-5 px-1'>
-                                <h2 className='text-center font-semibold text-white text-xs min-[575px]:text-sm min-[700px]:text-base min-[810px]:text-lg font-[inter]'>{slide.subHeading}</h2>
+                                <h2 className='text-center font-[inter] tracking-wide font-semibold  text-white text-xs min-[575px]:text-sm min-[700px]:text-base min-[810px]:text-xl '>{slide.subHeading}</h2>
                             </div>
                             <div className='flex justify-center mt-5'>
                                 <button onClick={() => handleScroll("contact")} className='px-8 sm:px-10 py-1.5 cursor-pointer hover:scale-105  rounded-lg text-white font-semibold bg-[#f59e0b] font-[inter] text-base sm:text-lg  max-[768px]:active:scale-95 transition duration-300'>Book Table</button>
@@ -54,6 +61,7 @@ const Hero = () => {
 
                 ))}
             </Swiper>
+
         </section>
     )
 }
