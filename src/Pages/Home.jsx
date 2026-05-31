936f2d3f437b2bda5dc5280519d915ece0a7db33
Agnibha Debnath <agnibhadebnath@gmail.com>
2026-05-31 10:23:39 +0530

    feat: add booking section to home page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,11 @@
-import { Route, Routes } from "react-router-dom"
+
 import Navbar from "../Components/Navbar"
 import Hero from "../Components/Hero"
 import Menu from "../Components/Menu"
 import About from "../Components/About"
 import Contact from "../Components/Contact"
 import Footer from "../Components/Footer"
+import Booking from "../Components/Booking"
 const Home = () => {
 
     return (
@@ -13,6 +14,7 @@ const Home = () => {
             <Hero />
             <Menu />
             <About />
+            <Booking />
             <Contact />
             <Footer />
         </>
